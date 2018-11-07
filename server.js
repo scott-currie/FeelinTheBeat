a2d28756ef271d5a03d26a47878df3491b27ca36
BehelitFam <mriodine@gmail.com>
2018-11-07 09:39:59 -0800

    build out file/page structure, added user Oauth for spotify, associated search/seed functionality

--- server.js
+++ server.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const querystring = require('query-string');
 const pg = require('pg');
-const querystring = require('querystring');
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
+const redirect_uri = 'http://localhost:3002/authRedirect';
 
 let spotify_token;
 
@@ -28,24 +29,27 @@ const visionClient = new vision.ImageAnnotatorClient({
 const app = express();
 app.use(cors());
 
-
+console.log('nu server.js')
 
 app.use(express.static('Public'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended:true}));
 
-
-app.get('/', getData);
 app.get('/vision', getGoogleVision);
 
 
 
-
 // Middleware pathing shite
-app.get('/', (req, res) => res.render('pages/index'));
+app.get('/', (req, res) => {
+  getToken(req, res)
+    .then(() => res.render('pages/index'))
+    .catch(err => res.send(err));
+});
 
-app.get('/auth', getToken);
+app.get('/auth', getUserAuth);
+
+app.get('/authRedirect', getUserToken)
 
 app.post('/search', renderSearch);
 
@@ -60,40 +64,93 @@ function getToken(req, res) {
     .send('grant_type=client_credentials')
     .set('Authorization', 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')))
     .then(results => {
-      console.log('got a nu token');
       process.env.SPOTIFY_CLIENT_TOKEN = results.body.access_token;
       spotify_token = results.body.access_token;
+      console.log('spotify token: ', spotify_token);
     })
     .catch(err => res.send(err));
 }
 
+function getUserAuth(req, res) {
+  let query = querystring.stringify({client_id: client_id, response_type: 'code', redirect_uri: redirect_uri});
+  res.redirect('https://accounts.spotify.com/authorize?' + query);
+}
+
+function getUserToken (req, res) {
+  let code = req.query.code;
+  console.log('entered getUserToken function');
+  return superagent
+    .post('https://accounts.spotify.com/api/token')
+    .set('Authorization', 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')))
+    .type('form')
+    .send(`code=${code}`)
+    .send(`redirect_uri=${redirect_uri}`)
+    .send('grant_type=authorization_code')
+    .then(response => {
+      let access_token = response.body.access_token;
+      let refresh_token = response.body.refresh_token;
+      res.render('pages/mood', {access_token: access_token, refresh_token: refresh_token});
+    })
+    .catch(err => {
+      console.log('getuserToken err');
+      res.send(err);
+    });
+}
+
+
 function renderSearch(req, res) {
   let danceability = req.body.danceability;
   let valence = req.body.valence;
-  res.render('pages/search', {danceability: danceability, valence: valence});
+  let access_token = req.body.access_token;
+  let refresh_token = req.body.refresh_token;
+  res.render('pages/search', {
+    danceability: danceability,
+    valence: valence,
+    access_token: access_token,
+    refresh_token: refresh_token
+  });
 }
 
 function spotifySearch(req, res) {
   console.log('in spotify search');
-  return getToken(req, res)
-    .then(() => {
-    //TODO: make dynamic with form results
-    let query = req.body.trackQuery;
-    let valence = req.body.valence;
-    let danceability = req.body.danceability;
-    return superagent
-      .get(`https://api.spotify.com/v1/search?q=${query}&type=track`)
-      .set('Authorization', 'Bearer ' + spotify_token)
-      .then(results => {
-        //TODO: put render functionality here; need to get image IDs, track ID, track name. Will send to our awesome results form. 
-        let trackResults = results.body.tracks.items.map(track => {
-          return {id: track.id, name: track.name, artists: track.artists.map(artist => artist.name), album_img_url: track.album.images[2].url};
-        });
-        res.render('pages/chooseSeeds', {trackResults: trackResults, danceability: danceability, valence: valence});
-      })
-      .catch(err => res.send(err));
+  let query = req.body.trackQuery;
+  let valence = req.body.valence;
+  let danceability = req.body.danceability;
+  let access_token = req.body.access_token;
+  let refresh_token = req.body.refresh_token;
+  console.log('spotify token: ', spotify_token);
+  return superagent
+    .get(`https://api.spotify.com/v1/search?q=${query}&type=track`)
+    .set('Authorization', 'Bearer ' + spotify_token)
+    .then(results => { 
+      console.log('got results');
+      return results.body.tracks.items.map(track => {
+        return {id: track.id, name: track.name, artists: track.artists.map(artist => artist.name), album_img_url: track.album.images[2].url};
+      });
     })
-    .catch(err => console.log('rick is gr8 but the token ain\'t'));
+    .then(trackResults => {
+      return superagent
+      .post('https://accounts.spotify.com/api/token')
+      .set('Authorization', 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')))
+      .send('grant_type=refresh_token')
+      .send(`refresh_token=${refresh_token}`)
+      .then(results => {
+        console.log('inside refresh logic');
+        if (results.status == '200') {
+          res.render('pages/chooseSeeds', {
+            trackResults: trackResults,
+            danceability: danceability,
+            valence: valence,
+            access_token: results.body.access_token,
+            refresh_token: refresh_token
+          });
+        } else {
+          throw 'a hissyfit';
+        }
+      })
+      .catch(err=> res.send(err));
+    })
+    .catch(err => res.send(err));
 }
 
 function makePlaylist(req, res) {
@@ -101,38 +158,42 @@ function makePlaylist(req, res) {
   // TODO:    1. Need to build mood-> track feature first
   // TODO:    2. Figure out how we can specify feature ranges to the Spotify API
   // TODO:    3. Return array of related tracks from API
-  let trackID = req.params.id;
-  let valence = req.body.valence / 100;
-  let paramRange = .1;
-  let max_valence = valence + paramRange;
-  let min_valence = valence - paramRange;
-  let danceability = req.body.danceability / 100;
-  return getToken(req, res)
+  let refresh_token = req.body.refresh_token,
+      access_token = req.body.access_token,
+      trackID = req.params.id,
+      valence = req.body.valence / 100,
+      paramRange = .2,
+      max_valence = valence + paramRange,
+      min_valence = valence - paramRange,
+      danceability = req.body.danceability / 100;
+  getToken(req, res)
     .then(() => {
-      let recs = spotifyRecs(req, res, max_valence, min_valence);
-      if (recs > 4) {
-        res.send({results: recs});
-        //res.render('pages/playlist', {results: recs});
-      } else {
-        max_valence += .1;
-        min_valence -= .1;
-        recs = spotifyRecs(req, res, max_valence, min_valence);
-        if (recs > 4) {
-          res.render('pages/playlist', {results: recs});
-        } else {
-          max_valence += .15;
-          min_valence -= .15;
-          recs = spotifyRecs(req, res, max_valence, min_valence);
-          if (recs > 4) {
+      spotifyRecs(req, res, max_valence, min_valence)
+        .then(recs => {
+          if (recs.length > 4) {
+            console.log(recs);
             res.render('pages/playlist', {results: recs});
           } else {
-            recs = spotifyRecs(req, res, 1, 0);
-            res.render('pages/playlist', {results: recs, response: 'We couldn\'t find results that fit those emotions. Here\'s a consolation playlist based off the track you selected.'})
+            max_valence += .1;
+            min_valence -= .1;
+            recs = spotifyRecs(req, res, max_valence, min_valence);
+            if (recs.length > 4) {
+              res.render('pages/playlist', {results: recs});
+            } else {
+              max_valence += .15;
+              min_valence -= .15;
+              recs = spotifyRecs(req, res, max_valence, min_valence);
+              if (recs.length > 4) {
+                res.render('pages/playlist', {results: recs, access_token: access_token, refresh_token});
+              } else {
+                recs = spotifyRecs(req, res, 1, 0);
+                res.render('pages/playlist', {results: recs, response: 'We couldn\'t find results that fit those emotions. Here\'s a consolation playlist based off the track you selected.'})
+              }
+            }
           }
-        }
-      }
+        })
     })
-    .catch(err => console.log('rick is gr8'));
+    .catch(err=> res.send(err));
 }
 
 
@@ -144,10 +205,10 @@ function spotifyRecs (req, res, max_valence, min_valence) {
     min_valence = 0.01;
   }
   return superagent
-      .get(`https://api.spotify.com/v1/recommendations?seed_tracks=${trackID}&max_valence=${max_valence}&min_valence=${min_valence}`)
-      .set('Authorization', 'Bearer ' + spotify_token)
-      .then(results => results.body.tracks.items)
-      .catch(err => res.send(err));
+    .get(`https://api.spotify.com/v1/recommendations?seed_tracks=${trackID}&max_valence=${max_valence}&min_valence=${min_valence}`)
+    .set('Authorization', 'Bearer ' + spotify_token)
+    .then(results => results.body.tracks.items)
+    .catch(err => res.send(err));
 }
 
 function AnnotatedImage(imageData) {
