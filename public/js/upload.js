79f4acc7dca28b4c50ffebfffcd479da127ce6b1
Scott Currie <vitriolik@gmail.com>
2018-11-09 08:54:33 -0800

    Added handling for all form submit buttons on upload and show pages

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,5 +1,21 @@
 'use strict';
 
+document.getElementById('chooseFileBtn').addEventListener('click', getFile);
+
 function getFile() {
   document.getElementById('uploadFile').click();
+  document.getElementById('uploadFileBtn').style.visibility = 'visible';
+  document.getElementById('uploadFileBtn').addEventListener('click', uploadFile);
+}
+
+// document.getElementById('uploadFileBtn').addEventListener('click', uploadFile);
+
+function uploadFile() {
+  document.getElementById('submitUploadForm').click();
+}
+
+document.getElementById('getPlaylistBtn').addEventListener('click', getPlaylist);
+
+function getPlaylist() {
+  document.getElementById('submitGetPlaylistForm').click();
 }
